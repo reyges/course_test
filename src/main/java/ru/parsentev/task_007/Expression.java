8a6f4678336ede1fdef78074ea24a17743e31ddf
Sergey <dev.yakovlev@gmail.com>
2016-08-07 21:22:54 +0300

    solve task 7

--- src/main/java/ru/parsentev/task_007/Expression.java
+++ src/main/java/ru/parsentev/task_007/Expression.java
@@ -3,6 +3,10 @@ package ru.parsentev.task_007;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
+import static java.lang.Math.*;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -21,6 +25,23 @@ public class Expression {
     }
 
     public double calc() {
-        throw new UnsupportedOperationException();
+
+        String operators[] = expr.split("[0-9]+");
+        String operands[] = expr.split("[^0-9]+");
+
+        if (operands.length < 2) throw new IllegalStateException();
+
+        for (int i = 0; i < operators.length; i++) {
+            if (operators[i].equals("+")) {
+                return Double.parseDouble(operands[i - 1]) + Double.parseDouble(operands[i]);
+            }
+            if (operators[i].equals("-")) {
+                return Double.parseDouble(operands[i - 1]) - Double.parseDouble(operands[i]);
+            }
+            if (operators[i].equals("exp")) {
+                return exp(Double.parseDouble(operands[i+1]));
+            }
+        }
+        return 0.0d;
     }
 }
