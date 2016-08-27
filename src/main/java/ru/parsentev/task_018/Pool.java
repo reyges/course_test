0ad509ad31a67cda8ac186224fd94289c25e5060
sergey <dev.yakovlev@gmail.com>
2016-08-27 16:13:40 +0300

    solve task 18

--- src/main/java/ru/parsentev/task_018/Pool.java
+++ src/main/java/ru/parsentev/task_018/Pool.java
@@ -16,11 +16,48 @@ public class Pool {
 
     private final int[][] values;
 
+    private int maxSumm = 0;
+    private int curSumm = 0;
+
+
     public Pool(final int[][] values) {
         this.values = values;
     }
 
     public int maxUnion() {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                curSumm = 0;
+                findSumm(i, j);
+                if (curSumm > maxSumm) {
+                    maxSumm = curSumm;
+                }
+            }
+        }
+
+        return maxSumm;
+    }
+
+    private void findSumm(int i, int j) {
+        if (values[i][j] == 1) {
+            curSumm++;
+            values[i][j]  = 0;
+            //up
+            if ((i > 0)) {
+                findSumm(i - 1, j);
+            }
+            //down
+            if (i < (values.length - 1)) {
+                findSumm(i + 1, j);
+            }
+            //left
+            if ((j > 0)) {
+                findSumm(i, j - 1);
+            }
+            //right
+            if ((j < values.length - 1)) {
+                findSumm(i, j + 1);
+            }
+        }
     }
 }
