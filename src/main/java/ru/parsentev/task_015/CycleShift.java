5825630d53b4910804537dbf5a5ea58d7599d7ec
sergey <dev.yakovlev@gmail.com>
2016-08-27 16:12:43 +0300

    solve task 15

--- src/main/java/ru/parsentev/task_015/CycleShift.java
+++ src/main/java/ru/parsentev/task_015/CycleShift.java
@@ -3,6 +3,8 @@ package ru.parsentev.task_015;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -21,6 +23,27 @@ public class CycleShift {
     }
 
     public int[] shift(int position) {
-        throw new UnsupportedOperationException();
+        int length = values.length;
+        if (position >= length) {
+            return values;
+        }
+        int[] result = new int[length];
+        int n = 0;
+        for (int i = position + 1; i < length; i++) {
+            result[n] = values[i];
+            n++;
+        }
+        for (int i = 0; i < position + 1; i++) {
+            result[n] = values[i];
+            n++;
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        CycleShift cycle = new CycleShift(new int[]{1, 2, 3, 4, 5});
+        int[] result = cycle.shift(2);
+        System.out.println(Arrays.toString(result));
     }
 }
