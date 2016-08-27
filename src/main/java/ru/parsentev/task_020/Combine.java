ad56b2615010e7082d5c6210965b5f6f5737bb4c
sergey <dev.yakovlev@gmail.com>
2016-08-27 16:14:37 +0300

    solve task 20

--- src/main/java/ru/parsentev/task_020/Combine.java
+++ src/main/java/ru/parsentev/task_020/Combine.java
@@ -1,8 +1,8 @@
 package ru.parsentev.task_020;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -17,12 +17,40 @@ public class Combine {
     private static final Logger log = getLogger(Combine.class);
 
     private final int[] values;
+    private List<List<Integer>> result = new ArrayList<List<Integer>>();
 
     public Combine(final int[] values) {
         this.values = values;
     }
 
     public List<List<Integer>> generate() {
-        throw new UnsupportedOperationException();
+        combine(values, 0);
+        return result;
     }
+
+
+    private void combine(int[] array, int i) {
+        if (i == array.length - 1) {
+            List<Integer> list = new ArrayList<Integer>();
+            for (int number: array){
+                list.add(number);
+            }
+            this.result.add(list);
+        } else {
+            for (int j = i; j < array.length; j++) {
+                swap(array, i, j);
+                combine(array, i + 1);
+                swap(array, i, j);
+            }
+        }
+    }
+
+    private void swap(int[] pa, int i, int j) {
+        int k = pa[i];
+        pa[i] = pa[j];
+        pa[j] = k;
+    }
+
+
+
 }
