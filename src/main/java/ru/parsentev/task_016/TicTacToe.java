63ac56fab305e045090e0eb9aa4322b43417093b
sergey <dev.yakovlev@gmail.com>
2016-08-27 16:13:00 +0300

    solve task 16

--- src/main/java/ru/parsentev/task_016/TicTacToe.java
+++ src/main/java/ru/parsentev/task_016/TicTacToe.java
@@ -21,6 +21,27 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        throw new UnsupportedOperationException();
+        boolean result = false;
+        final int length = values.length;
+        int countVertical = 0;
+        int countHorizont = 0;
+        int countDiagonal = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                countHorizont += values[i][j];
+                countVertical += values[j][i];
+                countDiagonal += values[j][j];
+            }
+            if (countHorizont == length) {
+                return true;
+            } else countHorizont = 0;
+            if (countVertical == length) {
+                return true;
+            } else countVertical = 0;
+            if (countDiagonal == length) {
+                return true;
+            }
+        }
+        return result;
     }
 }
