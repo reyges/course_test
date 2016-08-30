b39cc52dd742c7e12ba68d2c55cf0eef46ec6aee
yakov <dev.yakovlev@gmail.com>
2016-08-30 21:34:00 +0300

    remove getters

--- src/main/java/ru/parsentev/task_002/Point.java
+++ src/main/java/ru/parsentev/task_002/Point.java
@@ -21,14 +21,6 @@ public class Point {
     private final int x;
     private final int y;
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
     public Point(final int x, final int y) {
         this.x = x;
         this.y = y;
